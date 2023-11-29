451ff1c143b9b9fe8e09cd2ff51719b94e0036ec
WangLaoEr <suguguan@qq.com>
2023-11-30 01:32:17 +0800

    Add two plugins, hop and telescope.

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -16,5 +16,29 @@ keymap.set("n", "<leader>s", "<C-w>j")
 keymap.set("n", "<leader>d", "<C-w>l")
 
 keymap.set("n", "<leader>i", "<C-]>zz")
-keymap.set("n", "<leader>t", "<C-t>zz")
 keymap.set("n", "<leader>o", "<C-o>zz")
+
+-- hop
+local hop = require('hop')
+keymap.set("n", "<leader>g", ":HopWord<cr>")
+local directions = require('hop.hint').HintDirection
+keymap.set('', 'f', function()
+  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+end, {remap=true})
+keymap.set('', 'F', function()
+  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+end, {remap=true})
+keymap.set('', 't', function()
+  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+end, {remap=true})
+keymap.set('', 'T', function()
+  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
+end, {remap=true})
+
+
+-- telescope
+local builtin = require('telescope.builtin')
+keymap.set('n', '<leader>ff', builtin.find_files, {})
+keymap.set('n', '<leader>fg', builtin.live_grep, {})
+keymap.set('n', '<leader>fb', builtin.buffers, {})
+keymap.set('n', '<leader>fh', builtin.help_tags, {})
--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -44,4 +44,16 @@ return require('packer').startup(function(use)
 
 	use {"akinsho/toggleterm.nvim", tag = 'v2.*'}
 
+    use {
+      'nvim-telescope/telescope.nvim', tag = '0.1.4',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    use {
+      'phaazon/hop.nvim',
+      branch = 'v2', -- optional but strongly recommended
+      config = function()
+        -- you can configure Hop the way you like here; see :h hop-config
+        require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+      end
+    }
 end)
